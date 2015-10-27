a940d04882df7b2a4475b9c945841d05335b2943
Greg McKeon <greg@wl-10-190-85-86.wireless.duke.edu>
2015-10-27 15:09:13 -0400

    Initial Commit

---
+++ QuakeSpotter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuakeSpotter
+//
+//  Created by Greg McKeon on 10/27/15.
+//  Copyright © 2015 grm19. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
